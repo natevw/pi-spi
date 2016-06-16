natevw/pi-spi

commit a74f1ba266716e1a2782f5d09d182ee399797ee4

refactor spi transfer struct initialization to hopefully fix #21

spi_binding.cc
@@ -58,18 +58,11 @@ class SpiTransfer : public Nan::AsyncWorker {
           if (ret != -1) {
               ret = ioctl(fd, SPI_IOC_WR_LSB_FIRST, &order);
               if (ret != -1) {
-                  struct spi_ioc_transfer msg = {
-                      /*.tx_buf = */ (uintptr_t)buffer,
-                      /*.rx_buf = */ (uintptr_t)buffer,
-                      /*.len = */ buflen,
-                      /*.speed_hz = */ speed,
-                      
-                      // avoid "missing initializer" warnings…
-                      /*.delay_usecs = */ 0,
-                      /*.bits_per_word = */ 0,
-                      /*.cs_change = */ 0,
-                      /*.pad = */ 0,
-                  };
+                  struct spi_ioc_transfer msg = {};
+                  msg.tx_buf = buffer;
+                  msg.rx_buf = buffer;
+                  msg.len = buflen;
+                  msg.speed_hz = speed;
                   ret = ioctl(fd, SPI_IOC_MESSAGE(1), &msg);
               }
           }

commit 79243e2a40d8947a831accad8cf0a26d0e25903c

fix build regression introduced in a74f1ba266716e1a2782f5d09d182ee399797ee4, was looking at wrong struct definition…

spi_binding.cc
@@ -59,8 +59,8 @@ class SpiTransfer : public Nan::AsyncWorker {
               ret = ioctl(fd, SPI_IOC_WR_LSB_FIRST, &order);
               if (ret != -1) {
                   struct spi_ioc_transfer msg = {};
-                  msg.tx_buf = buffer;
-                  msg.rx_buf = buffer;
+                  msg.tx_buf = (uintptr_t)buffer;
+                  msg.rx_buf = (uintptr_t)buffer;
                   msg.len = buflen;
                   msg.speed_hz = speed;
                   ret = ioctl(fd, SPI_IOC_MESSAGE(1), &msg);